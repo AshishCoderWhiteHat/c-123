642cd86518b5a4697e8e8d8992aa1ad3f31d4276
AshishCoderWhiteHat <71976163+AshishCoderWhiteHat@users.noreply.github.com>
2021-05-09 20:45:23 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
 noseX = 0;
 noseY = 0;
-rightwrist = 0;
-leftwrist = 0;
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
 
 function setup() 
 {
     video = createCapture(VIDEO);
     video.size(550, 500);
-    canvas = createCanvas(550, 200);
-    Canvas.position(520, 200);
-    posenet = ml5.posenet(video, modelLoaded);
-    posenet.on('pose, gotposes');
+    canvas = createCanvas(550, 550);
+    canvas.position(560, 150);
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
 
 }
 
@@ -20,9 +21,25 @@ function modelLoaded() {
     console.log('posenet is inisalise')
 }
 
-function gotposes(results) {
+function gotPoses(results) {
 
     if(results.length > 0){
         console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("noseX = "  + noseX + "noseY = " + noseY);
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
     }
 }
+
+function draw() {
+
+    background('#969a97');
+    document.getElementById("square_side").innerHTML ="width and height will be " + difference + "px";
+    fill('00CC66');
+    stroke('#0066CC');
+    square(noseX , noseY , difference);
+}
+
